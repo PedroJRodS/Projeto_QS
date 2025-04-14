3ac331286151f7fc79da75e507e929c8fc1b0259
DevCarlosSilva <carlos.silva1704@aluno.ce.gov.br>
2025-04-13 23:30:24 -0300

    Lógica do crud de estado finalizado

---
+++ resources/views/condition_create.blade.php
@@ -0,0 +1,27 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Criar Estado') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <a href="{{ route('adminPanel') }}">Voltar</a>
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+
+                    @if (session()->has('message'))
+                    {{ session()->get('message'); }}
+                    @endif
+
+                    <form action="{{ route('conditions.store') }}" method="post">
+                        @csrf
+                        <input class="rounded" type="text" name="name" placeholder="Nome do estado"> <br>
+                        <button class="mt-1 rounded underline font-semibold" type="submit">Criar</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
